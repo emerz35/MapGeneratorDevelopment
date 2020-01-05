ba4e5f2e6768f69912d50e4962fd780d7e615c10
Emerz35 <charlie.h.hands@gmail.com>
2020-01-05 15:51:44 +0000

    AltitudeGeneration 2nd commit: Perlin noise works but doesn't look great

--- src/generation/altitudegenerators/PerlinNoiseAltitudeGenerator.java
+++ src/generation/altitudegenerators/PerlinNoiseAltitudeGenerator.java
@@ -10,25 +10,27 @@ import map.Point;
  */
 public class PerlinNoiseAltitudeGenerator implements AltitudeGenerator{
     
-    private final static double SPACING = 10;
+    private final static double SPACING = 50;
     private final static int MAX_ALTITUDE = 255,MIN_ALTITUDE = 0;
-    private final static double[][] Gs = new double[2][10];
-    private final static int[] Ps = new int[200]; 
+    private final static double[][] Gs = new double[20][2];
+    private final static int[] Ps = new int[2000]; 
     static{
-        for(double[] g:Gs){
+        for(int i = 0; i<Gs.length;i++){
             double j = Utils.R.nextDouble();
-            g = new double[]{Math.cos(2*Math.PI*j),Math.sin(2*Math.PI*j)};
-            
+            Gs[i] = new double[]{Math.cos(2*Math.PI*j),Math.sin(2*Math.PI*j)};
         }
-        for(int p:Ps){
-            p = Utils.R.nextInt();
+        System.out.println(Gs[1].length);
+        for(int i = 0;i<Ps.length;i++){
+            Ps[i] = Utils.R.nextInt(1000);
         }
     }
+    
+    
     @Override
     public Point[][] generate(Point[][] map) {
         for(int y = 0;y<map.length;y++){
             for(int x = 0; x<map[y].length;x++){
-                map[y][x].altitude = MIN_ALTITUDE + (int)(perlinNoiseAt(x,y)*(double)(MAX_ALTITUDE - MIN_ALTITUDE));
+                map[y][x].altitude = MIN_ALTITUDE + (int)((perlinNoiseAt(x,y)+1)/2d *(double)(MAX_ALTITUDE - MIN_ALTITUDE));
             }
         }
         return map;
@@ -46,13 +48,13 @@ public class PerlinNoiseAltitudeGenerator implements AltitudeGenerator{
                 g01 = Gs[Ps[i2 + Ps[j2+1]]%Gs.length],
                 g11 = Gs[Ps[i2 + 1 + Ps[j2+1]]%Gs.length],
                 g10 = Gs[Ps[i2 + 1 + Ps[j2]]%Gs.length];
-        
+       
         double n00 = dot(g00,dx,dy), n01 = dot(g01,dx,dy-1), n11 = dot(g11,dx-1,dy-1), n10 = dot(g10,dx-1,dy);
         
         dx = fade(dx);
         dy = fade(dy);
         
-        double nx0 = mix(n00,n01,dx), nx1 = mix(n01,n11,dx);
+        double nx0 = mix(n00,n10,dx), nx1 = mix(n01,n11,dx);
         
         return mix(nx0,nx1,dy);
     }
@@ -66,7 +68,7 @@ public class PerlinNoiseAltitudeGenerator implements AltitudeGenerator{
     }
     
     private double fade(double x){
-        return x*x*x*(x*(6*x-15)+10);
+        return x*x*x*(x*(x*6-15)+10);
     }
     
     private double mix(double a, double b, double t){
