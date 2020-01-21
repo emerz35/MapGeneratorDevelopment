c7db199ad0b9f1f532d065b3d526b805b9df8b24
Emerz35 <charlie.h.hands@gmail.com>
2020-01-21 11:37:01 +0000

    Altitude and Land Generation 9th commit: compacted land to middle Also added kernel - bug: land shows at edges

--- src/generation/imagegenerators/BiomeDependantImageGenerator.java
+++ src/generation/imagegenerators/BiomeDependantImageGenerator.java
@@ -15,7 +15,7 @@ public class BiomeDependantImageGenerator implements ImageGenerator{
         BufferedImage image = new BufferedImage(map[0].length,map.length,BufferedImage.TYPE_INT_RGB);
         for(int y = 0; y<map.length;y++){
             for(int x = 0; x<map[y].length;x++){
-                image.setRGB(x, y, map[y][x].biome.red *255*255+map[y][x].biome.green*255+map[y][x].biome.blue);
+                image.setRGB(x, y, map[y][x].biome.red *256*256+map[y][x].biome.green*256+map[y][x].biome.blue);
             }
         }
         return image;
