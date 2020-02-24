dfa9804bc4e90537d5001e910e61d09d781ebb3c
Emerz35 <charlie.h.hands@gmail.com>
2020-02-24 20:38:05 +0000

    Moving from pc to laptop

--- src/generation/biomegenerators/WhittakerBiomeGenerator.java
+++ src/generation/biomegenerators/WhittakerBiomeGenerator.java
@@ -1,6 +1,15 @@
 package generation.biomegenerators;
 
 import generation.BiomeGenerator;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import main.Utils;
+import map.Biome;
+import map.Centroid;
+import map.Map;
 import map.Point;
 
 /**
@@ -9,9 +18,38 @@ import map.Point;
  */
 public class WhittakerBiomeGenerator implements BiomeGenerator{
 
+    private static final String DIAGRAM = "images_test\\WhittakerDiagram.png";
+    
     @Override
     public Point[][] generate(Point[][] map) {
-        
+        Centroid[] centroids = Map.centroids;
+        return null;
+    }
+    
+    
+    private Biome getBiome(double temp, double precipitation){
+        try {
+            BufferedImage image = ImageIO.read(new File(DIAGRAM));
+            WritableRaster r = image.getRaster();
+            int[] pixel = new int[4];
+            
+            double x = 128d+9.4d*(30d-temp),y = 438d-0.89d*precipitation;
+            pixel = r.getPixel((int)x, (int)y, pixel);
+            
+            if(Utils.pixelEquals(pixel,8,250,54))return Biome.TROPICAL_RAINFOREST;
+            else if(Utils.pixelEquals(pixel,7,249,162))return Biome.TEMPERATE_RAINFOREST;
+            else if(Utils.pixelEquals(pixel,155,224,35))return Biome.SAVANNA;
+            else if(Utils.pixelEquals(pixel,250,148,24))return Biome.SUBTROPICAL_DESERT;
+            else if(Utils.pixelEquals(pixel,46,177,83))return Biome.TEMPERATE_DECIDUOUS_FOREST;
+            else if(Utils.pixelEquals(pixel,246,6,41))return Biome.WOODLAND;
+            else if(Utils.pixelEquals(pixel,246,220,1))return Biome.TEMPERATE_GRASSLANDS;
+            else if(Utils.pixelEquals(pixel,5,102,33))return Biome.TAIGA;
+            else if(Utils.pixelEquals(pixel,87,235,249))return Biome.TUNDRA;
+            return null;
+        } catch (IOException ex) {
+            //Logger.getLogger(WhittakerBiomeGenerator.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
     
 }
--- src/main/Utils.java
+++ src/main/Utils.java
@@ -1,6 +1,12 @@
 package main;
 
+import generation.biomegenerators.WhittakerBiomeGenerator;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
+import javax.imageio.ImageIO;
 
 public class Utils {
 
@@ -26,4 +32,24 @@ public class Utils {
         //return 1/(1+x/(s*s));
         return Math.exp(-x/(s*s));
     }
+    
+    public static boolean pixelEquals(int[] a,int... b){
+        for(int i=0;i<3;i++)if(a[i]!=b[i])return false;
+        return true;
+    }
+    /*
+    public static void main(String... args) throws IOException{
+        BufferedImage image = ImageIO.read(new File(WhittakerBiomeGenerator.DIAGRAM));
+        int[] pixel = new int[4];
+        WritableRaster r = image.getRaster();
+        for(int y = 0; y<image.getHeight();y++){
+            for(int x = 0;x<image.getWidth();x++){
+                pixel = r.getPixel(x, y, pixel);
+                if(pixelEquals(pixel,255,174,201))System.out.println(x+", "+y);
+                //System.out.println("ok");
+                //System.out.println(image.isAlphaPremultiplied());
+            }
+        }
+    }
+    */
 }
